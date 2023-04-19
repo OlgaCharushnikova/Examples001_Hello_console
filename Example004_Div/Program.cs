49f15b76215e353ea11054e6a2f23644706a42ec
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-04-19 14:37:22 +0300

    Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 13 ;
-double numberB = 2 ;
+﻿double numberA = new Random().Next(1, 10) ; // 0,1,2,3...9
+Console.WriteLine(numberA) ;
+double numberB = new Random().Next(1, 10) ; // 0,1,2,3...9
+Console.WriteLine(numberB) ;
 Console.WriteLine(numberA / numberB) ;
 
